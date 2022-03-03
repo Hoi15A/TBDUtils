85b5a0e04e5250b70b781610364ffb231ad1b52a
ByrtZ <taylor.byrne417@gmail.com>
2022-03-03 12:18:34 +0000

    Added --restart flag to /announcement.

--- src/main/kotlin/moe/neat/tbdutils/commands/Announcement.kt
+++ src/main/kotlin/moe/neat/tbdutils/commands/Announcement.kt
@@ -1,14 +1,10 @@
 package moe.neat.tbdutils.commands
 
-import cloud.commandframework.annotations.Argument
-import cloud.commandframework.annotations.CommandDescription
-import cloud.commandframework.annotations.CommandMethod
-import cloud.commandframework.annotations.CommandPermission
+import cloud.commandframework.annotations.*
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextDecoration
-import net.kyori.adventure.text.format.TextDecorationAndState
 import net.kyori.adventure.title.Title
 
 import org.bukkit.Bukkit
@@ -22,26 +18,46 @@ class Announcement : BaseCommand {
     @CommandMethod("announcement <text>")
     @CommandDescription("Displays title text to all players online")
     @CommandPermission("tbdutils.command.announcement")
-    fun announcement(@Argument("text") text: Array<String>) {
+    fun announcement(@Argument("text") text: Array<String>, @Flag("restart") isRestart: Boolean) {
         for(players in Bukkit.getServer().onlinePlayers) {
             players.playNote(players.location, Instrument.PLING, Note.sharp(1, Note.Tone.F))
 
-            players.sendMessage(Component.text("Announcement: ")
-                .color(NamedTextColor.YELLOW)
-                .decoration(TextDecoration.BOLD, true)
-                .append(Component.text(text.joinToString(" "))
-                    .color(NamedTextColor.WHITE).decoration(TextDecoration.BOLD, false)
+            if(isRestart) {
+                players.sendMessage(Component.text("Server Restarting: ")
+                    .color(NamedTextColor.RED)
+                    .decoration(TextDecoration.BOLD, true)
+                    .append(Component.text(text.joinToString(" "))
+                        .color(NamedTextColor.WHITE).decoration(TextDecoration.BOLD, false)
+                    )
                 )
-            )
 
-            players.showTitle(Title.title(
-                Component.text("Announcement").color(NamedTextColor.YELLOW),
-                Component.text(text.joinToString(" ")),
-                Title.Times.of(Duration.ofSeconds(1.toLong()),
-                    Duration.ofSeconds(6.toLong()),
-                    Duration.ofSeconds(1.toLong()))
+                players.showTitle(Title.title(
+                    Component.text("Server Restarting").color(NamedTextColor.RED),
+                    Component.text(text.joinToString(" ")),
+                    Title.Times.of(Duration.ofSeconds(1.toLong()),
+                        Duration.ofSeconds(6.toLong()),
+                        Duration.ofSeconds(1.toLong()))
+                    )
                 )
-            )
+            }
+            else {
+                players.sendMessage(Component.text("Announcement: ")
+                    .color(NamedTextColor.YELLOW)
+                    .decoration(TextDecoration.BOLD, true)
+                    .append(Component.text(text.joinToString(" "))
+                        .color(NamedTextColor.WHITE).decoration(TextDecoration.BOLD, false)
+                    )
+                )
+
+                players.showTitle(Title.title(
+                    Component.text("Announcement").color(NamedTextColor.YELLOW),
+                    Component.text(text.joinToString(" ")),
+                    Title.Times.of(Duration.ofSeconds(1.toLong()),
+                        Duration.ofSeconds(6.toLong()),
+                        Duration.ofSeconds(1.toLong()))
+                    )
+                )
+            }
         }
     }
 }
