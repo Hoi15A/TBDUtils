e1f3144590bbc802df925cf339356e22fc96afb7
ByrtZ <taylor.byrne417@gmail.com>
2022-03-03 22:25:05 +0000

    Changed minutes to minute(s) because I did a dumb and renamed the command because admins can't check commands before sending them.

--- src/main/kotlin/moe/neat/tbdutils/commands/RestartAnnouncement.kt
+++ src/main/kotlin/moe/neat/tbdutils/commands/RestartAnnouncement.kt
@@ -15,24 +15,24 @@ import java.time.Duration
 
 @Suppress("unused")
 class RestartAnnouncement : BaseCommand {
-    @CommandMethod("restartannouncement <time>")
+    @CommandMethod("announcerestart <time>")
     @CommandDescription("Displays restart title text to all players online")
-    @CommandPermission("tbdutils.command.restartannouncement")
-    fun announcement(@Argument("time") time: Int) {
+    @CommandPermission("tbdutils.command.announcerestart")
+    fun announcement(@Argument("time") time: UInt) {
         for(players in Bukkit.getServer().onlinePlayers) {
             players.playNote(players.location, Instrument.PLING, Note.sharp(1, Note.Tone.F))
 
             players.sendMessage(Component.text("Server restarting: ")
                 .color(NamedTextColor.RED)
                 .decoration(TextDecoration.BOLD, true)
-                .append(Component.text("In $time minutes.")
+                .append(Component.text("In $time minute(s).")
                     .color(NamedTextColor.WHITE).decoration(TextDecoration.BOLD, false)
                 )
             )
 
             players.showTitle(Title.title(
                 Component.text("Server restarting").color(NamedTextColor.RED),
-                Component.text("In $time minutes"),
+                Component.text("In $time minute(s)"),
                 Title.Times.of(Duration.ofSeconds(1.toLong()),
                     Duration.ofSeconds(3.toLong()),
                     Duration.ofSeconds(1.toLong()))
