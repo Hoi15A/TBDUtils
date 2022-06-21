23d64877b0383e7e607d5dc605b81025d2054821
Niklas <derNiklaas@users.noreply.github.com>
2022-06-21 11:30:03 +0200

    Replace old Hypixel API with the official one (#11)
    * Replace old Hypixel API with the official one
    
    https://github.com/HypixelDev/PublicAPI
    
    * Simplify caching logic and reorganize imports

--- src/main/kotlin/moe/neat/tbdutils/util/Hypixel.kt
+++ src/main/kotlin/moe/neat/tbdutils/util/Hypixel.kt
@@ -1,8 +1,8 @@
 package moe.neat.tbdutils.util
 
-import io.github.reflxction.hypixelapi.HypixelAPI
-import io.github.reflxction.hypixelapi.player.HypixelPlayer
-import io.github.reflxction.hypixelapi.player.PlayerRank
+import net.hypixel.api.HypixelAPI
+import net.hypixel.api.apache.ApacheHttpClient
+import net.hypixel.api.reply.PlayerReply
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.minimessage.MiniMessage
@@ -15,33 +15,41 @@ import java.util.*
  * @param apiKey Apikey obtained with /api on Hypixel
  */
 class Hypixel(apiKey: String) {
-    private val api = HypixelAPI.create(apiKey)
-    private val playerCache = mutableMapOf<UUID, HypixelPlayer>()
+    private val api = HypixelAPI(ApacheHttpClient(UUID.fromString(apiKey)))
+    private val playerCache = mutableMapOf<UUID, PlayerReply.Player>()
     private val mm = MiniMessage.miniMessage()
 
-    private fun getPlayer(player: UUID): HypixelPlayer {
-        if (playerCache[player] == null) {
-            playerCache[player] = api.getPlayer(player)
-        }
-        return playerCache[player]!!
-    }
+    private fun getPlayer(player: UUID) = playerCache.getOrPut(player) { api.getPlayerByUuid(player).get().player }
 
     /**
      * Get the formatted display name of a player on Hypixel.
      *
      * @param player The player for which to fetch the display name
      * @return Hypixel styled component
+     *
+     * @see <a href="https://github.com/HypixelDev/PublicAPI/wiki/Common-Questions#how-do-i-get-a-players-rank-prefix">Available Ranks on Hypixel</a>
      */
     fun getHypixelDisplayName(player: Player): Component {
         val defaultComponent = Component.text(player.name).color(NamedTextColor.GRAY)
-        val rank = getPlayer(player.uniqueId).newPackageRank ?: return defaultComponent
+        val apiPlayer = getPlayer(player.uniqueId)
 
-        return when (rank) {
-            PlayerRank.DEFAULT -> defaultComponent
-            PlayerRank.VIP -> Component.text("[VIP] ${player.name}").color(NamedTextColor.GREEN)
-            PlayerRank.VIP_PLUS -> mm.deserialize("<green>[VIP<gold>+</gold>] ${player.name}</green>")
-            PlayerRank.MVP -> Component.text("[MVP] ${player.name}").color(NamedTextColor.AQUA)
-            PlayerRank.MVP_PLUS -> mm.deserialize("<aqua>[MVP<red>+</red>] ${player.name}</aqua>")
+        return when (apiPlayer.highestRank) {
+            "VIP" -> Component.text("[VIP] ${player.name}").color(NamedTextColor.GREEN)
+            "VIP_PLUS" -> mm.deserialize("<green>[VIP<gold>+</gold>] ${player.name}</green>")
+            "MVP" -> Component.text("[MVP] ${player.name}").color(NamedTextColor.AQUA)
+            "MVP_PLUS" -> {
+                val plusColor = apiPlayer.selectedPlusColor
+                mm.deserialize("<aqua>[MVP<${plusColor}>+</${plusColor}>] ${player.name}</aqua>")
+            }
+            "SUPERSTAR" -> {
+                val tagColor = apiPlayer.superstarTagColor
+                val plusColor = apiPlayer.selectedPlusColor
+                mm.deserialize("<${tagColor}>[MVP<${plusColor}>++</${plusColor}>] ${player.name}</${tagColor}>")
+            }
+            "YOUTUBE" -> mm.deserialize("<red>[</red><white>YOUTUBE</white><red>] ${player.name}</red>")
+            "GAME_MASTER" -> Component.text("[GM] ${player.name}").color(NamedTextColor.DARK_GREEN)
+            "ADMIN" -> Component.text("[ADMIN] ${player.name}").color(NamedTextColor.RED)
+            else -> defaultComponent
         }
     }
 }
